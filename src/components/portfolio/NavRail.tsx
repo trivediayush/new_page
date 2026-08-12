d59260848c099965b21a0d55b12914dcd3360708
Ayush Trivedi <150885930+trivediayush@users.noreply.github.com>
2026-08-12 14:41:03 +0530

    Update NavRail.tsx

--- src/components/portfolio/NavRail.tsx
+++ src/components/portfolio/NavRail.tsx
@@ -44,7 +44,7 @@ export function NavRail() {
               aria-hidden="true"
               className="h-1.5 w-1.5 shrink-0 translate-y-[-0.1rem] bg-primary transition-transform duration-500 group-hover:scale-150"
             />
-            <span className="whitespace-nowrap">{profile.name}</span>
+            <span className="whitespace-nowrap">{~/$}</span>
           </a>
 
 
